fc47f767b5da3297376bc2dfbbca442c9e959737
SirMathhman <sirmathhman@gmail.com>
2026-09-01 01:09:27 -0400

    Adjust ESLint

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,19 @@ import tseslint from "typescript-eslint";
 import prettier from "eslint-config-prettier";
 
 export default tseslint.config(
-  { ignores: ["dist", "node_modules", "src-tauri"] },
+  {
+    ignores: ["dist", "node_modules", "src-tauri"],
+    rules: {
+      "max-lines": [
+        "warn",
+        {
+          max: 500,
+          skipBlankLines: true,
+          skipComments: true,
+        },
+      ],
+    },
+  },
   js.configs.recommended,
   ...tseslint.configs.recommended,
   prettier,
